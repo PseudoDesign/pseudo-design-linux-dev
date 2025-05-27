cfc10c383801e79b26226d52a91250ebd085769a
Adam Schafer <ams-tech@users.noreply.github.com>
2025-05-27 18:52:41 -0400

    add docker stuff

--- rakefile.rb
+++ rakefile.rb
@@ -1,4 +1,7 @@
 class Configuration
+    # "Configurations" allow users to set up local enviornment settings 
+    # to control exactly which system configuration they would like for
+    # their build environment.  See usage examples below.
     def initialize(name, default_value)
         @name = name
         @default_value = default_value
@@ -62,13 +65,17 @@ namespace :manifest do
 end
 
 namespace :docker do
+    desc "A string containing the environment variables used to map this rakefile with the docker module."
+    environment_vars = "DOCKER_FULL_IMAGE_NAME=#{docker_image_full_name}"
+
     desc "Build the development container. It will be tagged #{docker_image_full_name}"
     task :build do
-        `DOCKER_FULL_IMAGE_NAME=#{docker_image_full_name} ./docker/create-build-image.sh`
+        `#{environment_vars} ./docker/create-build-image.sh`
     end
 
     desc "(Re)start the development container with tag #{docker_image_full_name}"
     task :start do
-        `DOCKER_FULL_IMAGE_NAME=#{docker_image_full_name} ./docker/start-build-image.sh`
+        sh "#{environment_vars} ./docker/start-build-image.sh"
     end
 end
+
