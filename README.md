a6fc13146c5d9ce0c67ca63ccb334580c911db11
Adam Schafer <ams-tech@users.noreply.github.com>
2025-06-20 17:25:53 -0400

    rework instructions, functional up until set up development environment

--- README.md
+++ README.md
@@ -18,9 +18,13 @@ If you're new to Yocto, check out the [Intro to Yocto](docs/intro.md) document. 
 
 This describes the steps needed to set up and build images to run on the Cora Z7.  Details on these commands can be found elsewhere in the documentation.
 
+* Initialize the xilinx development environment: 
+    * `rake manifest:name[xilinx]`
+    * `rake manifest:sync`
+* Build the development container: `rake docker:build`
 * Bring up a shell in the development container: `rake docker:start`
-* Set up the development environment: `source scripts/setupsdk-xilinx workspace/scarthgap/xilinx`
-* Compile the minimal cora-z7 image: `MACHINE=cora-z7 bitbake petalinux-image-minimal`
+* Set up the development environment: `source scripts/setupsdk-${manifest-name} workspace/${manifest-branch}/${manifest-name}`
+* Compile the minimal cora-z7 image: `MACHINE=${machine-name} bitbake ${image}`
 
 ## `repo` development workflow
 
--- rakefile.rb
+++ rakefile.rb
@@ -3,15 +3,20 @@ class Configuration
     # to control exactly which system configuration they would like for
     # their build environment.  See usage examples below.
     def initialize(name, default_value)
+        # Args:
+        #   name - the name of the configuration
+        #   default_value - the value to use if the configuration isn't set.
         @name = name
         @default_value = default_value
     end
 
     def file
+        # Return the path to the file used to store this configuration.
         File.expand_path(File.join(File.dirname(__FILE__), ".conf", @name))
     end
 
     def value
+        # Provide the value of the configuration, or the default value if one was not provided.
         if File.exist?(self.file)
             File.read(self.file)
         else
@@ -20,6 +25,7 @@ class Configuration
     end
 
     def value=(value)
+        # Setter for this cofiguration.  Writes the provided value to the configuration file.
         FileUtils.mkdir_p File.dirname(self.file)
         File.write(self.file, value)
     end
@@ -30,6 +36,7 @@ workspace_dir = File.expand_path(File.join(File.dirname(__FILE__), "workspace"))
 docker_image_name = "pseudo-design-linux-dev"
 
 # A list of configurations and defaults
+manifest_repo_url = Configuration.new('manifest-repo-url', 'git@github.com:PseudoDesign/pseudo-design-linux-manifest.git')
 env_manifest_branch = Configuration.new('manifest-branch', 'scarthgap')
 env_manifest_name = Configuration.new('manifest-name', 'default')
 
@@ -39,9 +46,6 @@ env_id = "#{env_manifest_branch.value}-#{env_manifest_name.value}"
 # Derrived values for Docker
 docker_tag = env_id
 docker_image_full_name = "#{docker_image_name}:#{docker_tag}"
-
-# Derrived values for Docker
-yocto_build_directory_name = "build-#{env_id}"
 
 namespace :manifest do
     desc "Get or Set the manifest branch to the provided value."
@@ -60,18 +64,33 @@ namespace :manifest do
         puts "Manifest name: #{env_manifest_name.value}"
     end
 
-    desc "Initialize and sync the environment in the `workspace` directory using the `repo` command"
+    desc "Get or Set the manifest repository url to the provided value."
+    task :url, [:set_value] do |t, args|
+        if args.set_value
+            manifest_repo_url.value = args.set_value
+        end
+        puts "Manifest Repo URL: #{manifest_repo_url.value}"
+    end
+
+    desc "Initialize the environment in the `workspace` directory using the `repo` command"
+    task :init do
+        mkdir_p workspace_dir
+        Dir.chdir(workspace_dir) do
+            `repo init -u '#{manifest_repo_url.value}' -m '#{env_manifest_name.value}.xml' -b '#{env_manifest_branch.value}'`
+        end
+    end
+
+    desc "Shortcut for the `repo sync` command"
     task :sync do
         mkdir_p workspace_dir
         Dir.chdir(workspace_dir) do
-            `repo init -u 'git@github.com:PseudoDesign/pseudo-design-linux-manifest.git' -m '#{env_manifest_name.value}.xml' -b '#{env_manifest_branch.value}'`
             `repo sync`
         end
     end
 end
 
 namespace :docker do
-    desc "A string containing the environment variables used to map this rakefile with the docker module."
+    desc "A string containing the environment variables used to map this rakefile with the docker module.  See the called scripts for detials."
     environment_vars = "DOCKER_FULL_IMAGE_NAME=#{docker_image_full_name}"
 
     desc "Build the development container. It will be tagged #{docker_image_full_name}"
